2bc5d750037774c48c31c068da9ba4b39b2ae7ee
kws4211 <kws4211@naver.com>
2017-05-02 15:20:19 +0900

    zz

--- Hello/src/hello.java
+++ Hello/src/hello.java
@@ -4,8 +4,9 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
-		System.out.println("bye");
+		
 		System.out.println("hello");
+		
+		System.out.println("hell");
 	}
-
 }
