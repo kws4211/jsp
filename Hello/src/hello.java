5f2080ae5a5d643d9842336a2787586900fa2e6e
kws4211 <kws4211@naver.com>
2017-05-02 18:11:12 +0900

    123

--- Hello/src/hello.java
+++ Hello/src/hello.java
@@ -3,19 +3,18 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hello");
-		
-		System.out.println("hello");
-		
-		System.out.println("bye");
-		
-		System.out.println("bye");
 		System.out.println("hello");
 		
 		System.out.println("hello");
 		
 		System.out.println("bye");
 		
-		System.out.println("bye1234");
+		System.out.println("bye");
+		
+		System.out.println("hello");
+		
+		System.out.println("hello");
+		
+		System.out.println("bye");
 	}
 }
