127ffd65e4394e7e695ad6a957843b4ceec5ec9e
kosta-PC\kosta <kht518@naver.com>
2017-05-11 09:18:14 +0900

    컨트롤러 나누기 완료 xml 파일 수정

---
+++ bloSSOME2/src/blossome/control/AdminControl.java
@@ -0,0 +1,80 @@
+package blossome.control;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import blossome.command.Command;
+import blossome.command.CommandException;
+import blossome.command.CommandNull;
+import blossome.command.admin.CmdadQnaList;
+import blossome.command.appeal.CmdAppealList;
+import blossome.command.appeal.CmdAppealView;
+import blossome.command.tuk.CmdTukList;
+
+
+/**
+ * Servlet implementation class GuestControl
+ */
+public class AdminControl extends HttpServlet {
+	
+	private HashMap commandMap;
+	private String  error = "error.jsp";
+	private String dir = "/admin/";
+	
+
+    public AdminControl() {
+        super();       
+		initCommand();
+	}
+
+	private void initCommand(){
+		commandMap = new HashMap();
+		commandMap.put("adqna", new CmdadQnaList("adqna.jsp"));
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+	
+
+	private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+
+		String nextPage = "";
+		String cmdKey	= request.getParameter("cmd");//페이지 명을 보낼 파라미터 이름
+		if( cmdKey == null ){
+			cmdKey = "main";//메인 페이지 명
+		}
+		
+		Command cmd = null;
+
+		try{
+			if( commandMap.containsKey( cmdKey ) ){
+				cmd = (Command)commandMap.get( cmdKey );
+			}else{
+				throw new CommandException("지정할 명령어가 존재하지 않음");
+			}
+			nextPage = cmd.execute( request );
+			
+		}catch( CommandException e ){
+			request.setAttribute("javax.servlet.jsp.jspException", e );
+			nextPage = error;
+			System.out.println("오류 : " + e.getMessage() );
+		}
+		
+		RequestDispatcher reqDp = getServletContext().getRequestDispatcher( dir + nextPage );
+		reqDp.forward( request, response );
+		
+	}
+
+}
