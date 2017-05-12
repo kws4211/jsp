b2738c329cc3ca787fe4c50c7f1c0b5374fa0d38
kosta <kosta@kosta-PC>
2017-05-12 17:18:50 +0900

    오류 수정

--- bloSSOME2/src/blossome/command/member/CmdmemSignupinsert.java
+++ bloSSOME2/src/blossome/command/member/CmdmemSignupinsert.java
@@ -22,26 +22,26 @@ public class CmdmemSignupinsert implements Command{
 		MemVO vo = new MemVO();
 		
 		//받아온 값들을 vo의 내용값지정
-		vo.setMemid(request.getParameter("id"));
-		vo.setMemname(request.getParameter("name"));
-		vo.setMemnick(request.getParameter("nick"));
-		vo.setMempass(request.getParameter("pass"));
-		vo.setMembirth(request.getParameter("birth"));
-		vo.setMemtel(request.getParameter("tel"));
-		vo.setMemloc(request.getParameter("loc"));
-		vo.setMememail(request.getParameter("email"));
+//		vo.setMemid(request.getParameter("id"));
+//		vo.setMemname(request.getParameter("name"));
+//		vo.setMemnick(request.getParameter("nick"));
+//		vo.setMempass(request.getParameter("pass"));
+//		vo.setMembirth(request.getParameter("birth"));
+//		vo.setMemtel(request.getParameter("tel"));
+//		vo.setMemloc(request.getParameter("loc"));
+//		vo.setMememail(request.getParameter("email"));
 		String open = request.getParameter("Open");
 		
 		//상태 유형에 따른 숫자 지정
-		if(open.equals("승인안됨")){
-			vo.setMemstate(0);
-		}else if(open.equals("공개")){
-			vo.setMemstate(1);
-		}else if(open.equals("비공개")){
-			vo.setMemstate(2);
-		}else{
-			vo.setMemstate(3);
-		}
+//		if(open.equals("승인안됨")){
+//			vo.setMemstate(0);
+//		}else if(open.equals("공개")){
+//			vo.setMemstate(1);
+//		}else if(open.equals("비공개")){
+//			vo.setMemstate(2);
+//		}else{
+//			vo.setMemstate(3);
+//		}
 		SignupRepository repo = new SignupRepository();
 //		repo.insertSignup(vo);
 //		List<MemVO> list = repo.selbyIdlist(vo.getMemId());
